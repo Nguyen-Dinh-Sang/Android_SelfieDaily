25df41731dbc4c87db8620f79416286494040710
SSG\sangnd <sangnd@ssg.vn>
2021-05-16 05:02:20 +0700

    SelfieDaily-DinhSang: add permission

--- app/src/main/java/com/example/selfie/MainActivity.java
+++ app/src/main/java/com/example/selfie/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.selfie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.item_camera:
+
+                return true;
+            case R.id.item_setting:
+
+                return true;
+            case R.id.item_delete_selected:
+
+                return true;
+            case R.id.item_delete_all:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
