1d442654eb174daed692f696f1eeda496b554249
SSG\sangnd <sangnd@ssg.vn>
2021-05-16 11:18:23 +0700

    SelfieDaily-DinhSang: add select, delete

---
+++ app/src/main/java/com/example/selfie/ExampleService.java
@@ -0,0 +1,67 @@
+package com.example.selfie;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import static com.example.selfie.App.CHANNEL_ID;
+
+public class ExampleService extends Service {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String input = intent.getStringExtra("inputExtra");
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        String channelName = "My Background Service";
+        NotificationChannel chan = new NotificationChannel(CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        assert manager != null;
+        manager.createNotificationChannel(chan);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        Notification notification = notificationBuilder.setOngoing(true)
+                .setSmallIcon(R.drawable.ic_check_circle1)
+                .setContentTitle("App is running in background")
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .setContentIntent(pendingIntent)
+                .build();
+        startForeground(2, notification);
+
+        return START_NOT_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
--- app/src/main/java/com/example/selfie/MainActivity.java
+++ app/src/main/java/com/example/selfie/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.selfie;
 
+import androidx.annotation.LongDef;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
@@ -20,6 +21,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -41,18 +43,21 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView scrollView;
     private GridLayoutManager gridLayoutManager;
     private AdapterImageList adapterImageList;
+    private boolean isMultiSelected = false;
+    String TAG = "AAAAA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        adapterImageList = new AdapterImageList(this, listPathImage);
         scrollView = findViewById(R.id.picker);
         gridLayoutManager = new GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false);
         scrollView.setLayoutManager(gridLayoutManager);
 
         File folder = new File(Environment.getExternalStorageDirectory().toString() + "/Android/data/com.example.selfie/files/Pictures/");
 
-        if(folder.exists()) {
+        if (folder.exists()) {
             File[] allFiles = folder.listFiles(new FilenameFilter() {
                 public boolean accept(File dir, String name) {
                     return (name.endsWith(".jpg"));
@@ -64,9 +69,44 @@ public class MainActivity extends AppCompatActivity {
                 listPathImage.add(new Image(file.getAbsolutePath()));
             }
 
-            adapterImageList = new AdapterImageList(this, listPathImage);
             adapterImageList.setHasStableIds(true);
             scrollView.setAdapter(adapterImageList);
+
+            adapterImageList.setOnItemClickListener(new OnItemClickListener() {
+                @Override
+                public void onItemClick(int position) {
+                    if (position >= listPathImage.size()) return;
+
+                    if (!isMultiSelected) {
+                        dialogReview(listPathImage.get(position).getPath());
+                    } else {
+                        if (listPathImage.get(position).isSelected()) {
+                            listPathImage.get(position).setSelected(false);
+                            listSelectedImage.remove(listPathImage.get(position));
+                            if (listSelectedImage.size() < 1) {
+                                isMultiSelected = false;
+                            }
+                        } else {
+                            listPathImage.get(position).setSelected(true);
+                            listSelectedImage.add(listPathImage.get(position));
+                        }
+                        adapterImageList.notifyItemChanged(position);
+                    }
+                }
+
+                @Override
+                public void onItemLongClick(int position) {
+                    if (position >= listPathImage.size()) return;
+                    if (!isMultiSelected) {
+                        isMultiSelected = true;
+                        listPathImage.get(position).setSelected(true);
+                        adapterImageList.notifyItemChanged(position);
+                        listSelectedImage = null;
+                        listSelectedImage = new ArrayList<>();
+                        listSelectedImage.add(listPathImage.get(position));
+                    }
+                }
+            });
         }
     }
 
@@ -79,19 +119,43 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
         switch (item.getItemId()) {
             case R.id.item_camera:
                 dispatchTakePictureIntent();
                 return true;
             case R.id.item_setting:
-
+                dialogSetting();
                 return true;
             case R.id.item_delete_selected:
+                if (listSelectedImage != null) {
+                    for (Image image : listSelectedImage) {
+                        removedFile(image.getPath());
+                    }
+                }
 
+                File folder = new File(Environment.getExternalStorageDirectory().toString() + "/Android/data/com.example.selfie/files/Pictures/");
+                if (folder.exists()) {
+                    File[] allFiles = folder.listFiles(new FilenameFilter() {
+                        public boolean accept(File dir, String name) {
+                            return (name.endsWith(".jpg"));
+                        }
+                    });
+
+                    listPathImage.clear();
+                    for (File file : allFiles) {
+                        listPathImage.add(new Image(file.getAbsolutePath()));
+                    }
+
+                    isMultiSelected = false;
+                    adapterImageList.notifyDataSetChanged();
+                }
                 return true;
             case R.id.item_delete_all:
-
+                for (Image image : listPathImage) {
+                    removedFile(image.getPath());
+                }
+                listPathImage.clear();
+                adapterImageList.notifyDataSetChanged();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -117,43 +181,73 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            dialogReview();
+            dialogReview(null);
         }
     }
 
-    private void dialogReview() {
+    private void dialogReview(String link) {
+        Log.d(TAG, "dialogReview: " + link);
         Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.dialogs_review);
 
         ImageView imageView = dialog.findViewById(R.id.iv_review);
         TextView textViewOk = dialog.findViewById(R.id.tv_ok);
         TextView textViewReTake = dialog.findViewById(R.id.tv_retake);
-
-        Glide.with(this)
-                .load(currentPhotoPath)
-                .into(imageView);
+        if(link != null){
+            Glide.with(this)
+                    .load(link)
+                    .into(imageView);
+        } else {
+            Glide.with(this)
+                    .load(currentPhotoPath)
+                    .into(imageView);
+        }
 
         textViewOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                galleryAddPic();
-                dialog.dismiss();
+                if (link == null) {
+                    galleryAddPic();
+
+                    File folder = new File(Environment.getExternalStorageDirectory().toString() + "/Android/data/com.example.selfie/files/Pictures/");
+                    if (folder.exists()) {
+                        File[] allFiles = folder.listFiles(new FilenameFilter() {
+                            public boolean accept(File dir, String name) {
+                                return (name.endsWith(".jpg"));
+                            }
+                        });
+
+                        listPathImage.clear();
+                        for (File file : allFiles) {
+                            listPathImage.add(new Image(file.getAbsolutePath()));
+                        }
+                        adapterImageList.notifyDataSetChanged();
+                    }
+                    dialog.dismiss();
+                } else {
+                    dialog.dismiss();
+                }
             }
         });
 
         textViewReTake.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                removedFile(currentPhotoPath);
-                dispatchTakePictureIntent();
-                dialog.dismiss();
+                if (link == null) {
+                    removedFile(currentPhotoPath);
+                    dispatchTakePictureIntent();
+                    dialog.dismiss();
+                } else {
+                    dispatchTakePictureIntent();
+                    dialog.dismiss();
+                }
             }
         });
 
         dialog.show();
     }
 
-    private File createImageFile(){
+    private File createImageFile() {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
@@ -188,5 +282,37 @@ public class MainActivity extends AppCompatActivity {
         Uri contentUri = Uri.fromFile(f);
         mediaScanIntent.setData(contentUri);
         this.sendBroadcast(mediaScanIntent);
+    }
+
+    private void dialogSetting() {
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialogs_setting);
+
+        Switch switchSetting = dialog.findViewById(R.id.switch_setting);
+        TextView textViewOk = dialog.findViewById(R.id.tv_ok2);
+        textViewOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        switchSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean checked = ((Switch) v).isChecked();
+                if (checked) {
+                    Intent serviceIntent = new Intent(MainActivity.this, ExampleService.class);
+                    serviceIntent.putExtra("inputExtra", "ok");
+
+                    startService(serviceIntent);
+                } else {
+                    Intent serviceIntent = new Intent(MainActivity.this, ExampleService.class);
+                    stopService(serviceIntent);
+                }
+            }
+        });
+
+        dialog.show();
     }
 }
